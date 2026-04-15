9a7e77d67ebd64bb20099219d5e993466c64a8e6
amjadsh92 <amjadsharafeddien@gmail.com>
2026-04-15 17:28:16 +0300

    Add a hero image

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { archivo, lato } from './fonts';
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-black p-[1px]">
+    <div className="h-screen w-screen bg-[url('/home/images/project1.jpg')] bg-cover p-[1px]">
       <div className={`${archivo.className} text-[55px] text-white w-fit m-auto font-bold mt-[60px]`}> KAAN{" "}<span className={`${lato.className}`}>Architecten </span></div> 
      
     </div>
