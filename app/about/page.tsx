9ed867a72422a60cb078d8d3823c3549438c7d56
amjadsh92 <amjadsharafeddien@gmail.com>
2026-08-04 15:52:37 +0300

    Add a navbar

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -264,8 +264,58 @@ export default function About() {
           large-scale, versatile building in Aubervilliers, Paris. Currently, the office is engaged in several
           high-profile projects across Europe.
         </p>
-        {/* <div className="h-[500px]" /> */}
+        <div className="h-[300px]" />
       </div>
+
+      <div className="flex h-fit justify-center items-center max-[960px]:hidden">
+            <div
+              className={`${suisse.className} pt-6 w-fit text-black text-[10rem] font-[450] tracking-wide leading-none scale-x-125 translate-y-4`}
+            >
+              KAAN
+            </div>
+          </div>
+          <div className={`${suisse.className} flex justify-between items-center border-t w-full h-[2.5rem] p-[0.625rem] text-[0.75rem] max-[960px]:hidden`}>
+           
+            <div>CONTACT</div>
+            <div>NEWS ARCHIVE</div>
+            <div>INSTAGRAM</div>
+            <div>LINKEDIN</div>
+            <div>IMPRINT AND PRIVACY POLICY</div>
+            <div>@KAAN ARCHITECTEN</div>
+            
+
+          </div>
+
+
+
+
+           <div className={`${suisse.className} flex flex-col mt-[1.25rem] items-start justify-center border-t gap-[0.625rem] w-full p-[0.625rem] text-[0.75rem] max-[500px]:text-[0.6875rem] min-[960px]:hidden`}>
+           <div className="flex justify-between w-full">
+               <div>CONTACT</div>
+              <div>@KAAN ARCHITECTEN</div>
+
+           </div>
+        
+          
+            <div>NEWS ARCHIVE</div>
+            <div>INSTAGRAM</div>
+            <div>LINKEDIN</div>
+            <div>IMPRINT AND PRIVACY POLICY</div>
+             
+          
+            
+            
+            
+
+          </div>
+
+          <div className="flex h-fit justify-center items-center min-[960px]:hidden">
+            <div
+              className={`${suisse.className} pt-6 w-fit text-black text-[7.5rem] max-[650px]:text-[5.625rem] max-[500px]:text-[3.75rem] font-[450] tracking-wide leading-none scale-x-125 translate-y-4`}
+            >
+              KAAN
+            </div>
+          </div>
     </div>
   );
 }
